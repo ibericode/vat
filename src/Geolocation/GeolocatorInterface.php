d486f4bcc5834f029286fe39d03aab8c37fb8c17
Danny van Kooten <dannyvankooten@users.noreply.github.com>
2023-09-06 10:37:14 +0200

    fix code style

--- src/Geolocation/GeolocatorInterface.php
+++ src/Geolocation/GeolocatorInterface.php
@@ -2,12 +2,12 @@
 
 namespace Ibericode\Vat\Geolocation;
 
-interface GeolocatorInterface {
+interface GeolocatorInterface
+{
 
     /**
      * @param string $ipAddress The IP address to geolocate
      * @return string A ISO-3166-1-alpha2 country code or an empty string on failure
      */
     public function locateIpAddress(string $ipAddress) : string;
-
 }
